a6eb395b238c4492f34da826306c7d9759615a9e
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2023-12-28 13:40:35 +0100

    create html index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,6 @@
+<script setup>
+
+</script>
 <template>
     <h1>Département MMI</h1>
     <div> <!-- liens à mettre dans le menu -->
@@ -26,4 +29,84 @@
         <RouterLink to="/parcours-dev">Parcours dev</RouterLink>
         <p></p>
     </div>
+
+
+    <div class="hero">
+        <h1 class="hero__title">MMI MONTBÉLIARD</h1>
+        <p class="hero__paragraph">Vous former aux métiers du web : tel est l'objectif du BUT MMI. En trois ans, vous serez
+            capable de concevoir et de réaliser des produits et services multimédia en ligne.
+        </p>
+        <MyButton />
+        <div class="hero__blue-line"></div>
+        <img src="" alt="" class="hero__img">
+        <h2 class="hero__h2">Métiers du Multimédia & de l'Internet</h2>
+        <div class="hero__blue-line"></div>
+    </div>
+
+    <div class="cursus">
+        <p class="cursus__paragraph">Ce cursus dispensé en formation initiale ou en alternance à partir de la deuxième année
+            permet un accès direct à la vie professionnelle et autorise diverses poursuites d'études.</p>
+        <MyCursus />
+    </div>
+
+    <div class="parcours">
+        <h2 class="parcours_title"></h2>
+        <p class="parcours_body"></p>
+
+        <!-- Design -->
+        <MyParcours />
+        <!-- Com -->
+        <MyParcours />
+        <!-- Dev -->
+        <MyParcours />
+    </div>
+
+    <div class="decouvrir">
+        <h2 class="decouvrir__title">EN DÉCOUVRIR DAVANTAGE</h2>
+        <div class="decouvrir__arrow">
+            <svg></svg>
+            <p class="decouvrir_arrow--paragraph">Découvrez toutes les facettes de la formation.</p>
+        </div>
+        <!-- Espace Pro -->
+        <MyDecouvrir />
+        <!-- Département -->
+        <MyDecouvrir />
+        <!-- Formation -->
+        <MyDecouvrir />
+    </div>
+
+    <div class="international">
+        <h2 class="international__title--orange"> MMI À</h2>
+        <h2 class="international__title">L'INTERNATIONAL</h2>
+        <MyMap />
+
+        <div class="international__canada">
+            <p class="international__canada--body--bold">Canada</p>
+            <p class="international__canada--body">Montréal / Matane</p>
+        </div>
+        <div class="international__canada">
+            <p class="international__canada--body--bold">Espagne</p>
+            <p class="international__canada--body">Barcelone</p>
+        </div>
+        <div class="international__canada">
+            <p class="international__canada--body--bold">Inde</p>
+        </div>
+        <div class="international__canada">
+            <p class="international__canada--body--bold">Australie</p>
+        </div>
+        <div class="international__end">
+            <p>Notre BUT offre aux étudiants la possibilité de suivre un semestre complet à l'étranger dans des CÉGEP
+                (collège
+                d'enseignement général et professionnel) partenaires au <strong>Canada</strong>.
+
+                Les stages et les alternances sont également l'occasion de tenter la mobilité internationale.</p>
+
+            <MyButton />
+        </div>
+        <MyEllipse />
+        <MyEllipse />
+    </div>
 </template>
+
+
+<style lang="scss"></style>
