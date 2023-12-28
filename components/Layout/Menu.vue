c6b6bdc8fa37e3a5541befacd53be44c55eecddf
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2023-12-28 13:53:55 +0100

    add html menu.vue

--- components/Layout/Menu.vue
+++ components/Layout/Menu.vue
@@ -1,3 +1,63 @@
 <template>
-    Mon menu est bg
+    <!-- Mon menu est bg -->
+    <div class="menu">
+        <h1 class="menu_num">01</h1>
+        <div class="menu__right">
+            <div class="menu__text">
+                <p class="menu__title">La formation</p>
+                <p class="menu__paragraph">Découvrez le programme du BUT MMI </p>
+            </div>
+            <img src="" alt="" class="menu__img">
+        </div>
+        <div class="menu__line"></div>
+    </div>
+    <div class="menu">
+        <h1 class="menu_num">02</h1>
+        <div class="menu__right">
+            <div class="menu__text">
+                <p class="menu__title">Le département</p>
+                <p class="menu__paragraph">Découvrez le département où étudient les MMI</p>
+            </div>
+            <img src="" alt="" class="menu__img">
+        </div>
+        <div class="menu__line"></div>
+    </div>
+    <div class="menu">
+        <h1 class="menu_num">03</h1>
+        <div class="menu__right">
+            <div class="menu__text">
+                <p class="menu__title">Les extras</p>
+                <p class="menu__paragraph">Nous proposons de nombreuses opportunités à l'étranger</p>
+            </div>
+            <img src="" alt="" class="menu__img">
+        </div>
+        <div class="menu__line"></div>
+    </div>
+    <div class="menu">
+        <h1 class="menu_num">04</h1>
+        <div class="menu__right">
+            <div class="menu__text">
+                <p class="menu__title">Espace pro</p>
+                <p class="menu__paragraph">Vous avez un projet ? Vous cherchez
+                    Proposez vos offres aux étudiants</p>
+            </div>
+            <img src="" alt="" class="menu__img">
+        </div>
+        <div class="menu__line"></div>
+    </div>
+    <div class="menu">
+        <h1 class="menu_num">05</h1>
+        <div class="menu__right">
+            <div class="menu__text">
+                <p class="menu__title">Contact</p>
+                <p class="menu__paragraph">Contactez-nous pour plus d'informations sur le BUT MMI</p>
+            </div>
+            <img src="" alt="" class="menu__img">
+        </div>
+        <div class="menu__line"></div>
+    </div>
+
+    <MyButton />
 </template>
+
+<style lang="scss"></style>
