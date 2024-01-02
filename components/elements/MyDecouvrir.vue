ed561246b869f5101abbfc201359830ff8100705
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-02 17:20:56 +0100

    feat(decouvrir) : add mydecouvrir / mydecouvrirs

--- components/elements/MyDecouvrir.vue
+++ components/elements/MyDecouvrir.vue
@@ -1,7 +1,42 @@
 <script setup>
-
+defineProps({
+    id: String,
+    imageSrc: String,
+    imageAlt: String,
+    title: String,
+    href: String,
+});
 </script>
+
 <template>
-    decouvrir
+    <div class="decouvrir">
+        <div class="decouvrir_container">
+            <img class="decouvrir_container--image" :src="imageSrc" :alt="imageAlt" />
+        </div>
+
+        <RouterLink :to="href" class="decouvrir__link">
+            {{ title }}
+        </RouterLink>
+    </div>
 </template>
-<style ></style>
+
+<style lang="scss">
+.decouvrir {
+    text-align: center;
+
+    &__container {
+
+        &--image {
+            margin-top: 0;
+        }
+    }
+
+    &__link {
+        color: $black;
+        font-size: $body;
+        font-weight: bold;
+        text-decoration: none;
+        margin-top: rem(26);
+    }
+}
+</style>
---
+++ components/elements/MyDecouvrirs.vue
@@ -0,0 +1,48 @@
+<script setup>
+import MyParcour from './MyParcour.vue';
+
+import { reactive } from 'vue'
+const response = [
+    {
+        id: 1,
+        imageSrc: 'decouvrir_espace_pro.png',
+        imageAlt: 'decouvrir_espace_pro',
+        title: 'ESPACE PRO',
+        href: '/espace-pro',
+    },
+    {
+        id: 2,
+        imageSrc: 'decouvrir_departement.png',
+        imageAlt: 'decouvrir_departement',
+        title: 'DÉPARTEMENT',
+        href: '/departement',
+    },
+    {
+
+        id: 3,
+        imageSrc: 'decouvrir_formation.png',
+        imageAlt: 'decouvrir_formation',
+        title: 'FORMATION',
+        href: '/formation',
+    },
+];
+const decouvrirs = reactive(response)
+
+
+
+</script>
+<template>
+    <div class="mydecouvrirs">
+
+
+        <MyDecouvrir v-for="decouvrir in decouvrirs" v-bind='decouvrir' :key="decouvrir.id" />
+
+    </div>
+</template>
+<style lang="scss">
+.mydecouvrirs {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+
+}
+</style>
