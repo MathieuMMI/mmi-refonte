45da5e49862d572f511dccab7459397509a2087b
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-04 15:06:57 +0100

    feat(icon) : add MyCroix

---
+++ components/Icon/MyCroix.vue
@@ -0,0 +1,6 @@
+<template>
+    <svg width="41" height="40" viewBox="0 0 41 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M30.3301 10L10.3301 30" stroke="#FEFEFE" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M10.3301 10L30.3301 30" stroke="#FEFEFE" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+</template>
