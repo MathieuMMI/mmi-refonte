385e215d301e551292d09d8553cbb018cebc81c5
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-15 18:47:25 +0100

    footer contact

--- pages/project/[id].vue
+++ pages/project/[id].vue
@@ -8,17 +8,107 @@ const { data: projectpage } = await useAsyncData('project', () => client.getByID
 console.log(projectpage)
 // variables pour le contenu
 const titre = projectpage.value.data.project_title[0].text;
-const description = projectpage.value.data.project_desc[0].text ;
-const annee = projectpage.value.data.project_annee[0].text ;
-const image = projectpage.value.data.project_img.url ;
+const description = projectpage.value.data.project_desc[0].text;
+const annee = projectpage.value.data.project_annee[0].text;
+const image = projectpage.value.data.project_img.url;
 const imageALT = projectpage.value.data.project_img.alt;
 </script>
 
 <template>
-<div class="">
-    <h2 class="">{{ titre }}</h2>
-    <p class="">{{ description }}</p>
-    <img :src="image" :alt="imageALT" class="" />
-    <p>{{ annee }}</p>
+  <div class="projet">
+    <h2 class="projet__title">Projet</h2>
+    <h3 class="projet__subtitle">{{ titre }}</h3>
+    <p class="projet__date">{{ annee }}</p>
+    <!-- <a :href="link.url">Accéder au projet</a> -->
+    <p class="projet__description">{{ description }}</p>
+    <img :src="image" :alt="imageALT" class="projet__img" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.projet {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &__title {
+    color: $black;
+    font-size: $h2;
+    font-family: $font-satoshi-bold;
+
+  }
+
+  &__subtitle {
+    color: $grey;
+    font-size: $h3;
+    font-family: $font-satoshi-bold;
+  }
+
+  &__date {
+    color: $grey;
+    font-size: $h3;
+    font-family: $font-satoshi-bold;
+    margin-bottom: rem(90);
+  }
+
+  &__accroche {
+    color: $black;
+    font-size: $body;
+    font-family: $font-satoshi;
+    margin-bottom: rem(16);
+
+  }
+
+  &__description {
+    color: $black;
+    font-size: $body;
+    font-family: $font-satoshi;
+  }
+
+  &__img {}
+}
+
+@media screen and (max-width: 767px) {
+  .projet {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    &__title {
+      color: $black;
+      font-size: $mobile-h2;
+      font-family: $font-satoshi-bold;
+
+    }
+
+    &__subtitle {
+      color: $grey;
+      font-size: $mobile-body;
+      font-family: $font-satoshi-bold;
+    }
+
+    &__date {
+      color: $grey;
+      font-size: $mobile-body;
+      font-family: $font-satoshi-bold;
+      margin-bottom: rem(90);
+    }
+
+    &__accroche {
+      color: $black;
+      font-size: $mobile-body;
+      font-family: $font-satoshi;
+      margin-bottom: rem(16);
+
+    }
+
+    &__description {
+      color: $black;
+      font-size: $mobile-body;
+      font-family: $font-satoshi;
+    }
+
+    &__img {}
+  }
+}
+</style>
