21c6bcc64f627b82c8fe4ee988cba24444f98163
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-15 16:39:41 +0100

    responsive css contact

---
+++ components/Icon/MyMenuW.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg width="44" height="9" viewBox="0 0 44 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <ellipse cx="4.36711" cy="4.5" rx="4.36711" ry="4.5" fill="#ffffff" />
+        <ellipse cx="39.3046" cy="4.5" rx="4.36711" ry="4.5" fill="#ffffff" />
+        <ellipse cx="21.8359" cy="4.5" rx="4.36711" ry="4.5" fill="#ffffff" />
+    </svg>
+</template>
