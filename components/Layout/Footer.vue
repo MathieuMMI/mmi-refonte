97f404ac286a2eb2c52c148f3fbeef627fc033cd
cbeuchatmmi <95022983+cbeuchatmmi@users.noreply.github.com>
2024-01-15 17:59:21 +0100

    responsif css footer

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -19,46 +19,50 @@ const showFooterContent = route.name !== 'contact';
                     </p>
                     <p class="footer__adresse">4 place Tharradin 25 200 Montbéliard</p>
                     <p class="footer__tel">Tél : +33 (0)3 81 99 47 34 </p>
-                    <ul class="footer__liste">
-                        <li>
-                            <MyGoogle />
-                        </li>
-                        <li>
-                            <MyFacebook />
-                        </li>
-                        <li>
-                            <MyLinkedIn />
-                        </li>
-                        <li>
-                            <MyInstagram />
-                        </li>
-                        <li>
-                            <MyX />
-                        </li>
-                    </ul>
                 </div>
 
                 <ul class="footer__right">
-                    <li>
-                        <NuxtLink class="footer__right--link" to="/">Accueil</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink class="footer__right--link" to="/departement">Département</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink class="footer__right--link" to="/formation">La Formation</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink class="footer__right--link" to="/extras">Les Extras</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink class="footer__right--link" to="/contact">Contact</NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink class="footer__right--link" to="/faq">FAQ</NuxtLink>
-                    </li>
+                    <div>
+                        <li>
+                            <NuxtLink class="footer__right--link" to="/">Accueil</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink class="footer__right--link" to="/departement">Département</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink class="footer__right--link" to="/formation">La Formation</NuxtLink>
+                        </li>
+                    </div>
+                    <div>
+                        <li>
+                            <NuxtLink class="footer__right--link" to="/extras">Les Extras</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink class="footer__right--link" to="/contact">Contact</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink class="footer__right--link" to="/faq">FAQ</NuxtLink>
+                        </li>
+                    </div>
                     <li>
                         <NuxtLink class="footer__right--link" to="/mentions-legales">Mentions Légales</NuxtLink>
+                    </li>
+                </ul>
+                <ul class="footer__liste">
+                    <li>
+                        <MyGoogle />
+                    </li>
+                    <li>
+                        <MyFacebook />
+                    </li>
+                    <li>
+                        <MyLinkedIn />
+                    </li>
+                    <li>
+                        <MyInstagram />
+                    </li>
+                    <li>
+                        <MyX />
                     </li>
                 </ul>
             </div>
@@ -127,12 +131,95 @@ const showFooterContent = route.name !== 'contact';
     }
 
     &__right {
+
         &--link {
             text-decoration: none;
             color: $black;
 
             &:hover {
                 color: $primary-color; // Change to your desired hover color
+            }
+        }
+    }
+}
+
+@media screen and (max-width: 767px) {
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .line {
+        background-color: $secondary-color; // Adjust the color as needed
+        height: 3px;
+        width: 70%;
+        margin-bottom: rem(40);
+    }
+
+    .footer {
+        display: flex;
+        flex-direction: column;
+        justify-items: center;
+        align-items: center;
+        font-size: $mobile-body;
+
+        &__left {
+            margin-left: rem(20);
+            margin-bottom: rem(0);
+        }
+
+        &__title {
+            font-weight: bold;
+            margin-bottom: rem(20);
+            font-family: $font-satoshi-bold;
+        }
+
+        &__mail {
+            margin-bottom: rem(10);
+
+            &--to {
+                text-decoration: none;
+                color: $black;
+
+                &:hover {
+                    color: $primary-color; // Change to your desired hover color
+                }
+            }
+        }
+
+        &__adresse {
+            margin-bottom: rem(10);
+        }
+
+        &__tel {
+            margin-bottom: rem(20);
+        }
+
+        &__liste {
+            display: flex;
+            gap: rem(20); // Adjust the gap as needed
+        }
+
+        &__right {
+            display: flex;
+            gap: rem(32);
+            margin-bottom: rem(16);
+
+            li {
+                margin-top: rem(16);
+            }
+
+            &--link {
+                text-decoration: none;
+                color: $black;
+
+
+                &:hover {
+                    color: $primary-color; // Change to your desired hover color
+                }
             }
         }
     }
