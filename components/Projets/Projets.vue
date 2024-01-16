e64a43c9e152fe15fa0b6b7c507b1dc955fab096
MathieuMMI <mathieu.bouque@edu.univ-fcomte.fr>
2024-01-16 07:10:21 +0100

    feat: sorted projects

--- components/Projets/Projets.vue
+++ components/Projets/Projets.vue
@@ -1,15 +1,27 @@
 <script setup>
 const { client } = usePrismic()
 const { data: projects } = await useAsyncData('project', () => client.getAllByType('project'));
+const sort = [];
+
+Object.values(projects.value).forEach((project, index) => {
+    const projNumber = project.data.project_number[0].text;
+    console.log(projNumber);
+    sort.push({ project, projNumber });
+});
+
+// Triez le tableau sort en fonction des valeurs de projNumber
+sort.sort((a, b) => a.projNumber - b.projNumber);
+
+// Obtenez les projets triés
+const sortedProjects = sort.map(item => item.project);
 </script>
 
 <!-- Projets.vue -->
 <template>
   <div class="projects">
-    <Projet v-for="(project1, index) in projects.slice(0, 5)" :key="project1.project_id"
+    <Projet v-for="(project1, index) in sortedProjects.slice(0, 5)" :key="project1.project_id"
   :number="project1.data.project_number" :title="project1.data.project_title"
   :annee="project1.data.project_annee" :link="project1.data.project_link" :projectid="project1.id" />
-
   </div>
 </template>
   
@@ -17,6 +29,6 @@ const { data: projects } = await useAsyncData('project', () => client.getAllByTy
 <style>
 .projects {
   margin-top: 2rem;
-
+  white-space: nowrap;
 }
 </style>
