483c0265732bced563a1969fe24d9bd6aba2e678
MathieuMMI <mathieu.bouque@edu.univ-fcomte.fr>
2024-01-16 14:16:50 +0100

    feat: correction affichage footer

--- app.vue
+++ app.vue
@@ -1,15 +1,17 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
 const isFooterHidden = ref(route.name === 'contact' || (route.name.startsWith('en') && route.name !== 'en/'));
 const isHeaderVisible = ref(!route.fullPath.startsWith('/contact') && !route.fullPath.startsWith('/en/contact'));
+const isENFooterVisible = ref(route.name.startsWith('en') && route.name !== 'en/' && !route.fullPath.startsWith('/en/contact'));
 
 watchEffect(() => {
   isFooterHidden.value = route.name === 'contact' || (route.name.startsWith('en') && route.name !== 'en/');
   isHeaderVisible.value = !route.fullPath.startsWith('/contact') && !route.fullPath.startsWith('/en/contact');
-  });
+  isENFooterVisible.value = route.name.startsWith('en') && route.name !== 'en/' && !route.fullPath.startsWith('/en/contact');
+});
 </script>
 
 <template>
@@ -17,6 +19,6 @@ watchEffect(() => {
     <Header v-if="isHeaderVisible" />
     <NuxtPage />
     <Footer v-if="!isFooterHidden" />
-    <FooterEN v-if="isFooterHidden" />
+    <FooterEN v-if="isENFooterVisible" />
   </div>
 </template>
